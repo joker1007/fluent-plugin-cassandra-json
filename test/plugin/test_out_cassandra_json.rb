61a542195cf6f25f9e1b051811f06b5f5a107d82
joker1007 <kakyoin.hierophant@gmail.com>
2018-05-16 20:50:04 +0900

    Add test cases

--- test/plugin/test_out_cassandra_json.rb
+++ test/plugin/test_out_cassandra_json.rb
@@ -41,7 +41,110 @@ class CassandraJsonOutputTest < Test::Unit::TestCase
     assert { result.size == 1 }
 
     first = result.each.to_a[0]
-    assert { first == data.tap { |d| d["col6"] = Set.new(%w(one two three)) } }
+    expected = data.dup
+    expected["col6"] = Set.new(data["col6"])
+    assert { first["id"] == expected["id"] }
+    (1..7).each do |i|
+      assert { first["col#{i}"] == expected["col#{i}"] }
+    end
+  end
+
+  test "quoted data" do
+    driver = create_driver
+    time = Time.local(2018, 5, 8, 14, 10, 5)
+    data = {
+      "id" => 1,
+      "col1" => "text'data",
+      "col2" => time,
+      "col3" => true,
+      "col4" => 1.23,
+      "col5" => [1, 2, 3],
+      "col6" => ["one", "t\"wo", "th'r''ee"],
+      "col7" => {"key1" => "val1", "key2" => "val2"},
+    }
+    invalid_data = data.merge("id" => "invalid")
+    driver.run do
+      driver.feed("tag", Time.now.to_i, data)
+      driver.feed("tag", Time.now.to_i, data)
+      driver.feed("tag", Time.now.to_i, invalid_data)
+    end
+    result = @session.execute("SELECT * FROM test_keyspace.test_table")
+    assert { result.size == 1 }
+
+    first = result.each.to_a[0]
+    data["col6"] = Set.new(data["col6"])
+    assert { first["id"] == data["id"] }
+    (1..7).each do |i|
+      assert { first["col#{i}"] == data["col#{i}"] }
+    end
+  end
+
+  test "write data (if not exist)" do
+    conf = %[
+      hosts #{ENV.fetch("CASSANDRA_HOST", "127.0.0.1")}
+      port #{ENV.fetch("CASSANDRA_PORT", "9042")}
+      keyspace test_keyspace
+      table test_table
+      if_not_exists true
+    ]
+    driver = create_driver(conf)
+    time = Time.local(2018, 5, 8, 14, 10, 5)
+    data = {
+      "id" => 1,
+      "col1" => "textdata",
+      "col2" => time,
+      "col3" => true,
+      "col4" => 1.23,
+      "col5" => [1, 2, 3],
+      "col6" => ["one", "two", "three"],
+      "col7" => {"key1" => "val1", "key2" => "val2"},
+    }
+    driver.run do
+      driver.feed("tag", Time.now.to_i, data)
+    end
+    result = @session.execute("SELECT * FROM test_keyspace.test_table")
+    assert { result.size == 1 }
+
+    first = result.each.to_a[0]
+    data["col6"] = Set.new(data["col6"])
+    assert { first["id"] == data["id"] }
+    (1..7).each do |i|
+      assert { first["col#{i}"] == data["col#{i}"] }
+    end
+  end
+
+  test "write data (ttl)" do
+    conf = %[
+      hosts #{ENV.fetch("CASSANDRA_HOST", "127.0.0.1")}
+      port #{ENV.fetch("CASSANDRA_PORT", "9042")}
+      keyspace test_keyspace
+      table test_table
+      ttl 30
+    ]
+    driver = create_driver(conf)
+    time = Time.local(2018, 5, 8, 14, 10, 5)
+    data = {
+      "id" => 1,
+      "col1" => "textdata",
+      "col2" => time,
+      "col3" => true,
+      "col4" => 1.23,
+      "col5" => [1, 2, 3],
+      "col6" => ["one", "two", "three"],
+      "col7" => {"key1" => "val1", "key2" => "val2"},
+    }
+    driver.run do
+      driver.feed("tag", Time.now.to_i, data)
+    end
+    result = @session.execute("SELECT * FROM test_keyspace.test_table")
+    assert { result.size == 1 }
+
+    first = result.each.to_a[0]
+    data["col6"] = Set.new(data["col6"])
+    assert { first["id"] == data["id"] }
+    (1..7).each do |i|
+      assert { first["col#{i}"] == data["col#{i}"] }
+    end
   end
 
   private
